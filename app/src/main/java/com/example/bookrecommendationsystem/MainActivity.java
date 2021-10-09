235acaed4721395ba11db9248f53daaa9c8ee628
shantanu_joshi21 <shantanu.joshi2503@gmail.com>
2021-10-09 20:01:07 +0530

    New Activity named index is added using intent.

--- app/src/main/java/com/example/bookrecommendationsystem/MainActivity.java
+++ app/src/main/java/com/example/bookrecommendationsystem/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button index;
+    private Button index,explore,contribute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        explore=findViewById(R.id.explore);
+        explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Explore.class);
+                startActivity(intent);
+            }
+        });
+
+        contribute=findViewById(R.id.contribute);
+        contribute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Contribute.class);
+                startActivity(intent);
+            }
+        });
     }
 }

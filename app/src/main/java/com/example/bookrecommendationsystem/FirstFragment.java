16630e104d66240e5ae4fb815f802ea754c5d1d0
shantanu_joshi21 <shantanu.joshi2503@gmail.com>
2021-10-13 23:29:51 +0530

    First fragment also committed

--- app/src/main/java/com/example/bookrecommendationsystem/FirstFragment.java
+++ app/src/main/java/com/example/bookrecommendationsystem/FirstFragment.java
@@ -3,64 +3,61 @@ package com.example.bookrecommendationsystem;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.adapter.RecyclerViewAdapter;
 import com.example.bookrecommendationsystem.databinding.FragmentFirstBinding;
 
+import java.util.ArrayList;
+
 
 public class FirstFragment extends Fragment {
 
-    Button button;
-    TextView textView;
-    int cnt=0;
     FragmentFirstBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+
+    public ArrayList<Book> bookArrayList;
+    private ArrayAdapter<String> arrayAdapter;
 
     public FirstFragment() {
         // Required empty public constructor
     }
 
-
-    // TODO: Rename and change types and number of parameters
-   /* public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-    */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentFirstBinding.inflate(inflater,container,false);
-        Log.d("Fragment1 ","Created");
-        binding.Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cnt++;
-                Log.d("cnt",""+cnt);
-                binding.CountText.setText(""+cnt);
-            }
-        });
+
+        recyclerView= binding.recyclerView;
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        bookArrayList=new ArrayList<Book>();
+        for(int i=0;i<10;i++) {
+            bookArrayList.add(new Book("2 States","Cheatan Bhagat"));
+            bookArrayList.add(new Book("Gravity","Brahmagupta"));
+        }
+        recyclerViewAdapter=new RecyclerViewAdapter(getActivity(),bookArrayList);
+        recyclerView.setAdapter(recyclerViewAdapter);
 
         return binding.getRoot();
     }
